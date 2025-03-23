5789ce184301c34e62bef142b6195e96b8749055
Muhammad Usman Shamsi <usman.shamsi@live.com>
2025-03-23 15:58:58 +0500

    About form updated

--- Cracking_Torsion_Calculator/aboutForm.Designer.vb
+++ Cracking_Torsion_Calculator/aboutForm.Designer.vb
@@ -32,13 +32,13 @@ Partial Class aboutForm
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(12, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(366, 176)
+        Me.Label1.Size = New System.Drawing.Size(366, 128)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = resources.GetString("Label1.Text")
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(325, 228)
+        Me.Button1.Location = New System.Drawing.Point(325, 163)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -49,12 +49,12 @@ Partial Class aboutForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(412, 263)
+        Me.ClientSize = New System.Drawing.Size(412, 201)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "aboutForm"
